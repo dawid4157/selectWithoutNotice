a0ce3e088310290f792546549f4e36e457d57511
dawid4157 <dawid.ciosek@gmail.com>
2017-07-20 19:34:29 +0200

    Create class.select.php

---
+++ class.select.php
@@ -0,0 +1,33 @@
+<?php
+	/**
+	  * selectWithoutNotice
+	  * A class that helps display an error message while attempting to send a denied list item from HTML.
+	  * @version 1.0
+	  * @author dawid4157
+	  */
+	class Select {
+		private $paramName, $property = null;
+		/**
+		 * @param string $paramName
+	     * @return string
+	     */
+		public function getListName($paramName) {
+	        if (isset($_POST[$paramName])) {
+	            $this->property = trim($_POST[$paramName]);
+	        }
+	        return $this;
+	    }
+	    /**
+	     * @return $this
+	     */
+	    public function getListProperty() {
+	        return $this->property;
+	    }
+	    # Example validation
+	    public function doValid() {
+	    	if ($this->property == NULL) {
+	    		echo 'You must select fruit.';
+	    	}
+	    }
+	}
+?>
