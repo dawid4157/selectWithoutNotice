95a6d8ee8a791a8ac67c4d825a1cff211ce7781e
dawid4157 <dawid.ciosek@gmail.com>
2017-07-20 19:37:10 +0200

    Update class.select.php

--- class.select.php
+++ class.select.php
@@ -1,33 +1,32 @@
 <?php
+  /**
+    * selectWithoutNotice
+    * A class that helps display an error message while attempting to send a denied list item from HTML.
+    * @version 1.0
+    * @author dawid4157
+    */
+  class Select {
+     private $paramName, $property = null;
 	/**
-	  * selectWithoutNotice
-	  * A class that helps display an error message while attempting to send a denied list item from HTML.
-	  * @version 1.0
-	  * @author dawid4157
+	  * @param string $paramName
+	  * @return string
 	  */
-	class Select {
-		private $paramName, $property = null;
-		/**
-		 * @param string $paramName
-	     * @return string
-	     */
-		public function getListName($paramName) {
-	        if (isset($_POST[$paramName])) {
-	            $this->property = trim($_POST[$paramName]);
-	        }
-	        return $this;
-	    }
-	    /**
-	     * @return $this
-	     */
-	    public function getListProperty() {
-	        return $this->property;
-	    }
-	    # Example validation
-	    public function doValid() {
-	    	if ($this->property == NULL) {
-	    		echo 'You must select fruit.';
-	    	}
-	    }
+     public function getListName($paramName) {
+        if (isset($_POST[$paramName])) {
+	   $this->property = trim($_POST[$paramName]);
 	}
-?>
+	return $this;
+     }
+     /**
+       * @return $this
+       */
+     public function getListProperty() { 
+        return $this->property;
+     } 
+     # Example validation
+     public function doValid() {
+        if ($this->property == NULL) {
+	   echo 'You must select fruit.';
+	}
+    }
+}
